eae52ad358ad479b99b232741c9a134ea02d267e
a1kuat <88373996+a1kuat@users.noreply.github.com>
2021-08-04 17:01:38 +0500

    Update Bubblesort.cpp

--- Bubblesort.cpp
+++ Bubblesort.cpp
@@ -1,20 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std ;
 int main() {
-int n , a[10005] ;
+int n  ;
+vector <long long >v ;
 cin >> n ;
-for(int j = 1 ; j <= n ; j++){
-	cin >> a[j] ;
+for(int j = 0 ; j < n ; j++){
+	int x ;
+	cin >> x;
+	v.push_back(x) ;
 }
-for(int j = 1 ; j < n ; j++){
-	for(int i = 2 ; i <= n ; i++){
-		if(a[i] < a[i-1]){
-			swap(a[i] , a[i-1]) ;
+for(int j = 0 ; j < n-1 ; j++){
+	for(int i = 1 ; i < n ; i++){
+		if(v[i] < v[i-1]){
+			swap(v[i] , v[i-1]) ;
 		}
 	}
 }
-for(int j = 1 ; j <= n ; j++){
-	cout << a[j] << " " ;
+for(int j = 0; j < v.size() ; j++){
+	cout << v[j] << " " ;
 }
 return 0;
 }
